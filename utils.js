566288b2539150e754d2d8ad7bb2bc1920592ef5
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-05 17:31:11 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:30:54.554Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T14:31:11.631Z
+Commit: Fix CSS styling bug on mobile devices
