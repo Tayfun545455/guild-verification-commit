5df5c3ee9c33d0bce8643eee2ca2d327aecbc210
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-05 17:30:54 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:30:40.086Z
-Commit: Add dark mode support
+Timestamp: 2026-03-05T14:30:54.554Z
+Commit: Resolve null pointer exception in user service
