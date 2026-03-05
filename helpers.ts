961fef719678686062fa8c45560cc8664b4f108c
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-05 17:30:44 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:30:26.261Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T14:30:45.110Z
+Commit: Fix CSS styling bug on mobile devices
