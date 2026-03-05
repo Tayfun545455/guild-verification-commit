a5cd4f71b5fdb9bbc5c4334b9b17ac72a10b9309
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-05 17:28:52 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:28:48.200Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T14:28:53.189Z
+Commit: Patch API endpoint error handling
