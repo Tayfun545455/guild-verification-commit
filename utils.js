b4e83021a43f9161fb009077114042c18652e2e7
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-05 17:28:56 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:28:51.902Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T14:28:56.842Z
+Commit: Add multi-language support
