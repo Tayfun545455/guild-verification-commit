13e94533d3ccccd839258ccab335380fd35d73a3
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-05 17:28:43 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:28:33.099Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T14:28:43.540Z
+Commit: Fix memory leak in event handlers
