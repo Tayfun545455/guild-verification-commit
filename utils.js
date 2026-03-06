ed1ee87338516bd7358ec0bd2dd0c3ded42e6370
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-06 17:21:35 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:31:12.852Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-06T14:21:34.031Z
+Commit: Fix broken links in documentation
