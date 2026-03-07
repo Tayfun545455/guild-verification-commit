172fa39d4a5e3f413f6c6ea37d687b375b98ce4e
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-07 16:22:22 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T13:22:18.910Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-07T13:22:22.550Z
+Commit: Fix CSS styling bug on mobile devices
