9a593644a4b4461641b8dfb593567c717795b564
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-07 16:22:53 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T13:21:56.826Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-07T13:22:54.161Z
+Commit: Resolve null pointer exception in user service
