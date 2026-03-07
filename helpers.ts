a373e65d455927a429d70153c10b36beab59b83e
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-07 16:22:20 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T13:22:04.647Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-07T13:22:21.100Z
+Commit: Add dark mode support
