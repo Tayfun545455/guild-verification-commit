18a6a38f000cc8e469ea80bb1fa9e278df8bcedb
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-07 16:22:04 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T13:22:00.422Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-07T13:22:04.647Z
+Commit: Patch security vulnerability in authentication
