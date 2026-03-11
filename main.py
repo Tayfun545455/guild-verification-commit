cba6031b6fb16b5ca163296fdecc11a866fb0f37
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-11 18:34:06 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T09:49:41.571Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-11T15:34:05.910Z
+Commit: Fix CSS styling bug on mobile devices
