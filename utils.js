edafac50ba343bb237b460387799ba10e2fe7371
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-11 18:33:46 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T09:49:37.897Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-11T15:33:46.133Z
+Commit: Fix CSS styling bug on mobile devices
