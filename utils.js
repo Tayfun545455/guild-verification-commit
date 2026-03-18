0ab210dbd5b2607769965d905ec66ac532342253
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-18 05:35:51 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T15:33:59.516Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-18T02:35:50.689Z
+Commit: Fix race condition in async operations
