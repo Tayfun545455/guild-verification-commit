30fa9f95e4e90621e8374be78460833e6ce4e3e7
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-20 08:22:15 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T05:22:10.178Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-20T05:22:15.310Z
+Commit: Fix broken links in documentation
