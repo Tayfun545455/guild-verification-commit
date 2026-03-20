bb0e83c8a630fefee046a9909defd70a226ef23f
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-20 08:22:18 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T05:22:15.310Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-20T05:22:18.450Z
+Commit: Fix memory leak in event handlers
