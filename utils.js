38c506ebbd4ebee478fe478eaae39e6adce066fb
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-23 21:25:26 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-22T07:48:56.182Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-23T18:25:25.474Z
+Commit: Resolve null pointer exception in user service
