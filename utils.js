ecf8480002e310d44ed3e57aa8c762db061e602d
Tayfun545455 <tayfunsk54@gmail.com>
2026-03-29 10:28:03 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T07:27:56.572Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-29T07:28:03.133Z
+Commit: Introduce real-time notifications
